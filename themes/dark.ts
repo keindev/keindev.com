13fc1987516e4f539595847603f3120a17ab1801
Daniil Ryazanov <kein@tagproject.ru>
2019-12-08 22:38:33 +0300

    feat: add theme

--- themes/dark.ts
+++ themes/dark.ts
@@ -3,6 +3,20 @@ import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
+    typography: {
+        fontFamily: [
+            '"Lato"',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
     palette: {
         primary: {
             main: '#15202B',
